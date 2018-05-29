bd56e00736f2752e3098efe6c7b67b34af861d57
Corean Gonzales <coreangonzales@gmail.com>
2018-05-29 08:48:26 -0500

    completed media page

--- page-media.php
+++ page-media.php
@@ -1,63 +1,32 @@
 <?php get_header(); ?>
 <main class="middle-area">
 <section id="photos" >
-    <div class="container">
+  <div class="container">
     <div class="row">
-      <div class="col-md-4">
-        <div class="thumbnail">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/fivehoopkick.jpg"
-            onclick="openModal();currentSlide(1)" alt="broadway hula hoop" style="width:100%">
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="thumbnail">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/five.jpg" onclick="openModal();currentSlide(2)"  alt="circus hula hoop" style="width:100%">
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="thumbnail">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/balljuggle.jpg" onclick="openModal();currentSlide(3)" alt="juggler" style="width:100%">
-        </div>
-      </div>
-     </div>
-    <div class="row">
-     <div class="col-md-4">
-       <div class="thumbnail">
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/sixhoop.jpg" onclick="openModal();currentSlide(4)" alt="cirque hula hoop" style="width:100%">
-       </div>
-     </div>
-     <div class="col-md-4">
-       <div class="thumbnail">
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/jugglehoop.jpg" onclick="openModal();currentSlide(5)" alt="hula hoop juggling" style="width:100%">
-       </div>
-     </div>
-     <div class="col-md-4">
-       <div class="thumbnail">
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/abdc.jpg" onclick="openModal();currentSlide(6)" alt="LED hula hooping" style="width:100%">
-       </div>
-     </div>
-   </div>
-    <div class="row">
-     <div class="col-md-4">
-       <div class="thumbnail">
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/clubs.jpg" onclick="openModal();currentSlide(7)" alt="club juggling" style="width:100%">
-       </div>
-     </div>
-     <div class="col-md-4">
-       <div class="thumbnail">
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/graduationevent.png" onclick="openModal();currentSlide(8)" alt="hula hoop show" style="width:100%">
-       </div>
-     </div>
-     <div class="col-md-4">
-       <div class="thumbnail">
+          <?php
+            /* The loop */
+            while ( have_posts() ) : the_post();
+                if ( get_post_gallery() ) :
+                    $gallery = get_post_gallery( get_the_ID(), false );
 
-           <img src="<?php echo get_template_directory_uri(); ?>/assets/sixflags.jpg" onclick="openModal();currentSlide(9)" alt="event entertainment" style="width:100%">
+                    /* Loop through all the image and output them one by one */
+                    $slidenumber = 1;
 
-       </div>
-     </div>
+                    foreach( $gallery['src'] as $src ) : ?>
+                    <div class="col-md-4">
+                      <div class="thumbnail">
+                        <img src="<?php echo $src; ?>" class="my-custom-class" onclick="openModal();currentSlide(<?php echo $slidenumber; ?>)" alt="Gallery image" />
+                      </div>
+                    </div>
+                        <?php
+                        $slidenumber ++;
+                    endforeach;
+                endif;
+            endwhile;
+          ?>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
   <section id="myModal" class="modal">
     <div class="container">
@@ -67,59 +36,28 @@
         </div>
       </div>
       <div class="row modal-content">
+        <div class="row">
+            <?php
+            while ( have_posts() ) : the_post();
+                if ( get_post_gallery() ) :
+                    $gallery = get_post_gallery( get_the_ID(), false );
 
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/fivehoopkick.jpg" style="width:100%">
-          </div>
+                    /* Loop through all the image and output them one by one */
+                    foreach( $gallery['src'] as $src ) : ?>
+                    <div class="col-xs-12 mySlides">
+                        <img src="<?php echo $src; ?>" class="my-custom-class" style="width:100%" alt="Gallery image" />
+                    </div>
+                        <?php
+                    endforeach;
+                endif;
+            endwhile;
+            ?>
         </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/five.jpg" style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/balljuggle.jpg" style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/sixhoop.jpg"  style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/jugglehoop.jpg"  style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/abdc.jpg" style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/clubs.jpg" style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/graduationevent.png" style="width:100%">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 mySlides">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/sixflags.jpg"  style="width:100%">
-          </div>
-        </div>
-
         <!-- Controls -->
         <a class="prev" onClick="plusSlides(-1)"> < </a>
         <a class="next" onclick="plusSlides(1)"> > </a>
       </div>
     </div>
-
   </section>
 
 
